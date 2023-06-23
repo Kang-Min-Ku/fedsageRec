6ede77b41d4e4eb2115a3df27be0cb812363165a
Kang-Min-Ku <netisen4@gmail.com>
2023-06-24 01:53:32 +0900

    corroect config fit for NGCF

--- src/util/config.py
+++ src/util/config.py
@@ -1,4 +1,6 @@
 import torch
+import torch.optim as optim
+
 root_path = "/home/netisen2/gdve/baseline/fedsagerec/"
 no_cuda = False
 cuda = not no_cuda and torch.cuda.is_available()
@@ -6,10 +8,16 @@ device = "cuda" if cuda else "cpu"
 #cuda=False
 
 dataset = "ml-100k"
-num_owners = 10
+num_owners = 2
 delta = 20
 load_data = root_path+"dataowner/"+dataset
 global_data_ratio = 0.5
+
+use_early_stopping = True
+early_stopping_patience = 10
+early_stopping_policy = "ndcg" # recall, ndcg, hit
+test_every = 2
+print_every = 30
 
 num_samples = [5, 5]
 batch_size = 2048
@@ -17,18 +25,29 @@ latent_dim = 64
 steps = 10
 epochs_local = 100
 lr = 0.001
+regs = 1e-3
 weight_decay = 1e-4
 hidden = 32
 dropout = 0.15
 n_layers = 2
+mess_dropout = [0.10]*n_layers
+layers = [32,32]
+optimizer = optim.Adam
+bpr_decay = 1e-3
 
 gen_epochs = 10
-num_pred = 30
+num_pred = 40
 hidden_portion = 0.15
 top_k = 100
 
-epoch_recommendation = 100
+epoch_recommendation = 1024
 recommendation_layer_sizes = [64, 32]
+
+communication_round = 40
+classifier_valid_ratio = 0.1
+communication_test_every = 1
+communication_print_every = 4
+aggregate_exception = ["user_emb", "item_emb"]
 
 local_test_acc_dir = root_path+'local_result/test_acc/' + dataset+"_"+str(num_owners)
 global_test_acc_file = root_path+'global_result/test_acc/' + dataset+"_"+str(num_owners)
